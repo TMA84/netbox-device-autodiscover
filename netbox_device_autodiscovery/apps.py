2c0f1d7bb0be62837f126b44c362f9a94dd0569c
Tobias Malcherek <tobias.malcherek@gmail.com>
2026-01-18 19:02:41 +0100

    Fix apps.py configuration

--- netbox_device_autodiscovery/apps.py
+++ netbox_device_autodiscovery/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class DeviceAutoDiscoveryAppConfig(AppConfig):
     name = 'netbox_device_autodiscovery'
     verbose_name = 'Device Auto-Discovery'
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         import netbox_device_autodiscovery.signals
